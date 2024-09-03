6ccecbe1c8864939ce012ff0666f01ee34139147
Anna Boeva <anna.boeva16@gmail.com>
2024-09-03 22:22:50 +0300

    Create app

--- src/main/java/ru/tbank/Main.java
+++ src/main/java/ru/tbank/Main.java
@@ -1,7 +1,21 @@
 package ru.tbank;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        logger.info("Application started");
+
+        Location location = Location.fromJsonFile("/city.json");
+        if (location != null) {
+            logger.info("Location object created: {}", location);
+            location.saveToXMLFile("location.xml");
+        } else {
+            logger.warn("Location object creation failed");
+        }
+
+        logger.info("Application finished");
     }
 }
