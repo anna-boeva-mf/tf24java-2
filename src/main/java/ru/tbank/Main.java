3232875cf17a284f358dbc0fa0e606c173b42092
Anna Boeva <anna.boeva16@gmail.com>
2024-09-10 22:02:42 +0300

    ДЗ-3

--- src/main/java/ru/tbank/Main.java
+++ src/main/java/ru/tbank/Main.java
@@ -1,7 +1,37 @@
 package ru.tbank;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // 1
+        CustomLinkedList<Integer> list0 = new CustomLinkedList<>();
+        list0.add(1);
+        System.out.println(list0);
+        System.out.println(list0.get(0));
+        System.out.println(list0.contains(1));
+
+        list0.remove(0);
+        System.out.println(list0);
+        System.out.println(list0.contains(1));
+
+        Integer[] array = {1, 2, 3, 4, 5};
+        ArrayList<Integer> arrList = new ArrayList<>(Arrays.asList(array));
+        list0.addAll(arrList);
+        System.out.println(list0);
+
+        // 2
+        Stream<Integer> stream = Stream.of(6, 7, 8, 9, 10);
+        CustomLinkedList<Integer> list = stream.reduce(new CustomLinkedList<>(), (acc, element) -> {
+            acc.add(element);
+            return acc;
+        }, (cll, col) -> {
+            cll.addAll((Collection<? extends Integer>) col);
+            return cll;
+        });
+        System.out.println(list);
     }
 }
